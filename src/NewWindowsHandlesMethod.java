8fd6564de227f743d1272f2acad02bc85376546d
samurza38 <smurza9@gmail.com>
2019-10-30 16:31:02 -0600

    55 th commit

---
+++ src/NewWindowsHandlesMethod.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class NewWindowsHandlesMethod
+{
+   public static void main(String[] args){
+       InvokeBrowser myBrowser=new InvokeBrowser();
+       WebDriver driver=myBrowser.invokeBrowser();
+       driver.get("http://accounts.google.com/signup");
+       driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+       driver.findElement(By.xpath("//a[text()='Terms']")).click();
+       int i=0;
+       Set<String> id=driver.getWindowHandles();
+       for (String windowValue : id){
+           i++;
+
+           driver.switchTo().window(windowValue);
+           System.out.println(i+". windows title is:"+driver.getTitle());
+       }
+   }
+}
