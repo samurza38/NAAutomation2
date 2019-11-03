e43eac3cb08d325115d941f6cf9460e533413723
samurza38 <smurza9@gmail.com>
2019-11-03 16:26:41 -0700

    59 th commit

--- src/ExcelRead.java
+++ src/ExcelRead.java
@@ -9,16 +9,22 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class ExcelRead
-{
-   public static void main(String[]args) throws IOException {
-       File file=new File("C:\\Users\\Abi Auto\\Downloads\\Try.xls");
-       FileInputStream fs=new FileInputStream(file);
-       Workbook wb=new HSSFWorkbook(fs);
-       Sheet sh=wb.getSheet("Sheet1");
-       Row  rw=sh.getRow(1);
-       Cell celll=rw.getCell(1);
-       System.out.println(celll.getStringCellValue());
-   }
-}
+public class ExcelRead {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Users\\Abi Auto\\Downloads\\Try.xls");
+        FileInputStream fs = new FileInputStream(file);
+        Workbook wb = new HSSFWorkbook(fs);
+        Sheet sh = wb.getSheet("Sheet1");
+        int rowCount = sh.getLastRowNum();
+        Row rw;
+        Cell cell;
+        for (int i = 0; i < rowCount; i++) {
+            rw=sh.getRow(i);
+            cell=rw.getCell(0);
 
+            //Row  rw=sh.getRow(1);
+            //Cell celll=rw.getCell(1);
+            System.out.println(cell.getStringCellValue());
+        }
+    }
+}
